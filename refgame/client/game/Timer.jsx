0992b739461a4bb79a3f63dcf2abd776412f1e22
dussiaslab <dussiaslab@gmail.com>
2023-04-19 11:51:52 -0400

    update timer

--- refgame/client/game/Timer.jsx
+++ refgame/client/game/Timer.jsx
@@ -8,8 +8,8 @@ class timer extends React.Component {
     const { remainingSeconds } = this.props;
     const minutes = ("0" + Math.floor(remainingSeconds / 60)).slice(-2);
     const seconds = ("0" + (remainingSeconds - minutes * 60)).slice(-2);
-  
-  
+
+
     const classes = ["timer", "bp3-card"];
     if (remainingSeconds <= 5) {
       classes.push("lessThan5");
@@ -19,7 +19,7 @@ class timer extends React.Component {
 
     return (
       <div className={classes.join(" ")}>
-        <h5 className='bp3-heading'>Timer</h5>
+        <h5 className='bp3-heading'>Contador</h5>
         <span className="seconds">{minutes}:{seconds}</span>
       </div>
     );
